7ca64c27c7e94a5ce41d59ea3e2d00a113fd517c
Crited <Alexander.Stoll@netways.de>
2019-07-22 15:23:21 +0200

    Expanded upon Icinga_ProviderWindows.psm1; Minor Changes to other modules regarding structure and comments

--- lib/provider/windows/Icinga_ProviderWindows.psm1
+++ lib/provider/windows/Icinga_ProviderWindows.psm1
@@ -1,11 +1,42 @@
-function Show-IcingaWindowsData()
+Import-Module $IncludeDir\provider\enums\Icinga_ProviderEnums;
+
+function Get-IcingaWindows()
 {
     $WindowsInformations = Get-CimInstance Win32_OperatingSystem;
 
     $windows_datails = @{};
-    foreach($cpu_core in $WindowsInformations.CimInstanceProperties) {
-        $windows_datails.Add($cpu_core.Name, $cpu_core.Value);
-    }
+    $windows_datails.Add(
+        'windows', @{
+            'metadata' = @{
+                'Version' = $WindowsInformations.Version;
+                'CurrentTimeZone' = $WindowsInformations.CurrentTimeZone;
+                'InstallDate' = $WindowsInformations.InstallDate;
+                'SystemDevice' = $WindowsInformations.SystemDevice;
+                'SystemDirectory' = $WindowsInformations.SystemDirectory;
+                'BuildType' = $WindowsInformations.BuildType;
+                'BuildNumber' = $WindowsInformations.BuildNumber;
+                'OSArchitecture' = $WindowsInformations.OSArchitecture;
+                'NumberOfUsers' = $WindowsInformations.NumberOfUsers;
+                'OSType' = @{
+                    'raw' = $WindowsInformations.OSType;
+                    'value' = $ProviderEnums.WindowsOSType[[int]$WindowsInformations.OSType];
+                };
+                'OSProductSuite' = @{
+                    'raw' = $WindowsInformations.OSProductSuite;
+                    'value' = $ProviderEnums.WindowsOSProductSuite[[int]$WindowsInformations.OSProductSuite];
+                };
+                'ProductType' = @{
+                    'raw' = $WindowsInformations.ProductType;
+                    'value' = $ProviderEnums.WindowsProductType[[int]$WindowsInformations.ProductType];
+                };
+            };
+            'language' = @{
+                'CountryCode' = $WindowsInformations.CountryCode;
+                'OSLanguage' = $WindowsInformations.OSLanguage;
+                'Locale' = $WindowsInformations.Locale;
+            }
+        }
+    );
 
     return $windows_datails;
 }
