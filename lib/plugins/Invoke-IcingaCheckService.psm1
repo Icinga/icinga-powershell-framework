a5ab649f80f088d13d2d09b7a560132e08f39007
Alexander Stoll <astoll@int.netways.de>
2019-07-24 10:26:39 +0200

    Expanded upon lib/provider/services to support translated status values in Invoke-IcingaCheckService.psm1

--- lib/plugins/Invoke-IcingaCheckService.psm1
+++ lib/plugins/Invoke-IcingaCheckService.psm1
@@ -13,10 +13,10 @@ function Invoke-IcingaCheckService()
     $FoundService = Get-IcingaServices -Service $Service;
     $ServiceName  = $FoundService.Values.metadata.ServiceName;
     $DisplayName  = $FoundService.Values.metadata.DisplayName;
-   # $Status       = Get-IcingaServicesStatusTranslation -Status $Status;
+    $Status       = Get-IcingaServicesStatusTranslation -Status $Status;
     $StatusRaw    = $FoundService.Values.configuration.Status.raw;
 
-    $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Service "{0} ({1})"', $DisplayName, $ServiceName)) -Value $StatusRaw -ObjectExists $FoundService -ValueTranslation $ProviderEnums.ServiceStatus;
+    $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Service "{0} ({1})"', $DisplayName, $ServiceName)) -Value $StatusRaw -ObjectExists $FoundService -ValueTranslation $ProviderEnums.ServicesStatus;
     $IcingaCheck.CritIfNotMatch($Status) | Out-Null;
 
     exit (New-IcingaCheckResult -Name "Service $Service" -Check $IcingaCheck -NoPerfData $TRUE -Compile);
