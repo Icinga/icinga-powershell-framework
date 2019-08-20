2cf532d07ee28d93d704e5d8cae324c9e8fc3d56
Lord Hepipud <contact@lordhepipud.de>
2019-08-20 17:02:58 +0200

    Added check for Windows Uptime

---
+++ lib/plugins/Invoke-IcingaCheckUptime.psm1
@@ -0,0 +1,27 @@
+Import-IcingaLib icinga\plugin;
+Import-IcingaLib provider\windows;
+Import-IcingaLib core\tools;
+
+function Invoke-IcingaCheckUptime()
+{
+    param(
+        $Warning,
+        $Critical,
+        [switch]$NoPerfData,
+        [int]$Verbose
+    );
+
+    $WindowsData = Get-IcingaWindows;
+    $Name        = ([string]::Format('Windows Uptime: {0}', (ConvertFrom-TimeSpan -Seconds $WindowsData.windows.metadata.uptime.value)));
+
+    $IcingaCheck = New-IcingaCheck -Name 'Windows Uptime' -Value $WindowsData.windows.metadata.uptime.value -Unit 's';
+    $IcingaCheck.WarnOutOfRange(
+        (ConvertTo-SecondsFromIcingaThresholds -Threshold $Warning)
+    ).CritOutOfRange(
+        (ConvertTo-SecondsFromIcingaThresholds -Threshold $Critical)
+    ) | Out-Null;
+
+    $CheckPackage = New-IcingaCheckPackage -Name $Name -OperatorAnd -Checks $IcingaCheck -Verbose $Verbose;
+
+    exit (New-IcingaCheckresult -Check $CheckPackage -NoPerfData $NoPerfData -Compile);
+}
