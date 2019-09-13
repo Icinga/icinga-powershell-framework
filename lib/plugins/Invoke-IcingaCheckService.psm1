f0267ada70371e420c4b34d5ae53a3663c18f375
Alexander Stoll <astoll@int.netways.de>
2019-09-13 15:59:45 +0200

    Added Description for Invoke-IcingaCheckService

--- lib/plugins/Invoke-IcingaCheckService.psm1
+++ lib/plugins/Invoke-IcingaCheckService.psm1
@@ -2,6 +2,44 @@ Import-IcingaLib provider\services;
 Import-IcingaLib provider\enums;
 Import-IcingaLib icinga\plugin;
 
+<#
+.SYNOPSIS
+   Checks if a service has a specified status.
+
+.DESCRIPTION
+   Invoke-icingaCheckService returns either 'OK' or 'CRITICAL', if a service status is matching status to be checked.
+
+   More Information on https://github.com/LordHepipud/icinga-module-windows
+
+.FUNCTIONALITY
+   This module is intended to be used to check whether one or more services have a certain status. 
+   As soon as one of the specified services does not match the status, the function returns 'CRITICAL' instead of 'OK'.
+
+.EXAMPLE
+   PS>Invoke-IcingaCheckService -Service WiaRPC, Spooler -Status '1' -Verbose 3
+   [CRITICAL]: Check package "Services" is [CRITICAL] (Match All)
+    \_ [OK]: Service "Ereignisse zum Abrufen von Standbildern (WiaRPC)" is Stopped
+    \_ [CRITICAL]: Service "Druckwarteschlange (Spooler)" Running is not matching Stopped
+
+.PARAMETER Service
+   Used to specify an array of services which should be checked against the status.
+   Seperated with ','
+
+.PARAMETER Status
+   Status for the specified service or services to check against.
+
+ .INPUTS
+   System.Array
+
+ .OUTPUTS
+   System.String
+
+ .LINK
+   https://github.com/LordHepipud/icinga-module-windows
+
+ .NOTES
+#>
+
 function Invoke-IcingaCheckService()
 {
     param(
