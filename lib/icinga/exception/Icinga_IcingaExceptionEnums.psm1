7d533513cbf396b97a68065d7c171ee36e22663e
Lord Hepipud <contact@lordhepipud.de>
2019-09-14 16:12:59 +0200

    Added missing exception definitions

--- lib/icinga/exception/Icinga_IcingaExceptionEnums.psm1
+++ lib/icinga/exception/Icinga_IcingaExceptionEnums.psm1
@@ -4,8 +4,12 @@
  # entire components
  #>
 
- [hashtable]$Throw = @{
-    PerformanceCounter = 'Icinga failed to fetch Performance Counter information. This may be caused when the Icinga Service User is not permited to access these information. To fix this, please add the User the Icinga Agent is running on into the "Performance Log Users" group.';
+[hashtable]$Permission = @{
+    PerformanceCounter = 'A Plugin failed to fetch Performance Counter information. This may be caused when the used Service User is not permitted to access these information. To fix this, please add the User the Icinga Agent is running on into the "Performance Log Users" group.';
+};
+
+[hashtable]$Inputs = @{
+    PerformanceCounter = 'A plugin failed to fetch Performance Counter information. Please ensure the counter is written properly and available on your system.';
 };
 
 <#
@@ -16,7 +20,8 @@
  # $IcingaExceptionEnums.IcingaExecptionHandlers.PerformanceCounter
  #>
 [hashtable]$IcingaExceptions = @{
-    Throw = $Throw;
+    Permission = $Permission;
+    Inputs     = $Inputs;
 }
 
 Export-ModuleMember -Variable @( 'IcingaExceptions' );
