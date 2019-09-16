1597b0afcfe4679890fd95e5e7722cc13513e7b6
Lord Hepipud <contact@lordhepipud.de>
2019-09-16 17:32:25 +0200

    Added tool function to increase digit count
    * Added new function Format-IcingaDigitCount to fill digits with leading 0 depending on missing values
    * Added this function to CPU check

--- lib/plugins/Invoke-IcingaCheckCPU.psm1
+++ lib/plugins/Invoke-IcingaCheckCPU.psm1
@@ -1,4 +1,5 @@
 Import-IcingaLib core\perfcounter;
+Import-IcingaLib core\tools;
 Import-IcingaLib icinga\plugin;
 
 function Invoke-IcingaCheckCPU()
@@ -16,12 +17,12 @@ function Invoke-IcingaCheckCPU()
 
     if ($CpuCounter.Counters.Count -ne 0) {
         foreach ($counter in $CpuCounter.Counters) {
-            $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}', $counter.Instance)) -Value $counter.Value().Value -Unit '%';
+            $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}', (Format-IcingaDigitCount $counter.Instance -Digits 3))) -Value $counter.Value().Value -Unit '%';
             $IcingaCheck.WarnOutOfRange($Warning).CritOutOfRange($Critical) | Out-Null;
             $CpuPackage.AddCheck($IcingaCheck);
         }
     } else {
-        $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}',$Core)) -Value $CpuCounter.Value().Value -Unit '%';
+        $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}', (Format-IcingaDigitCount $Core -Digits 3))) -Value $CpuCounter.Value().Value -Unit '%';
         $IcingaCheck.WarnOutOfRange($Warning).CritOutOfRange($Critical) | Out-Null;
         $CpuPackage.AddCheck($IcingaCheck);
     }
