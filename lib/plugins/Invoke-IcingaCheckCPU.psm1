66d007f476c08462bbdad41e122a2872c23de92f
Lord Hepipud <contact@lordhepipud.de>
2019-09-16 17:59:12 +0200

    Replaced 0 with space for digits and auto added spacing

--- lib/plugins/Invoke-IcingaCheckCPU.psm1
+++ lib/plugins/Invoke-IcingaCheckCPU.psm1
@@ -17,12 +17,12 @@ function Invoke-IcingaCheckCPU()
 
     if ($CpuCounter.Counters.Count -ne 0) {
         foreach ($counter in $CpuCounter.Counters) {
-            $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}', (Format-IcingaDigitCount $counter.Instance -Digits 3))) -Value $counter.Value().Value -Unit '%';
+            $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core {0}', (Format-IcingaDigitCount $counter.Instance -Digits ([string](Get-IcingaCpuCount)).Length -Symbol ' '))) -Value $counter.Value().Value -Unit '%';
             $IcingaCheck.WarnOutOfRange($Warning).CritOutOfRange($Critical) | Out-Null;
             $CpuPackage.AddCheck($IcingaCheck);
         }
     } else {
-        $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core #{0}', (Format-IcingaDigitCount $Core -Digits 3))) -Value $CpuCounter.Value().Value -Unit '%';
+        $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Core {0}', (Format-IcingaDigitCount $Core -Digits ([string](Get-IcingaCpuCount)).Length -Symbol ' '))) -Value $CpuCounter.Value().Value -Unit '%';
         $IcingaCheck.WarnOutOfRange($Warning).CritOutOfRange($Critical) | Out-Null;
         $CpuPackage.AddCheck($IcingaCheck);
     }
