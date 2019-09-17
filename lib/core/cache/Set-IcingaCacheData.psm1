5f4c78886129e55f6c9578b1a7c3fc445340db4c
Lord Hepipud <contact@lordhepipud.de>
2019-09-17 17:17:38 +0200

    Added support to create and load cache data (prototype)

---
+++ lib/core/cache/Set-IcingaCacheData.psm1
@@ -0,0 +1,32 @@
+function Set-IcingaCacheData()
+{
+    param(
+        [string]$Space,
+        [string]$CacheStore,
+        [string]$KeyName,
+        $Value
+    );
+
+    $CacheFile = Join-Path -Path (Join-Path -Path (Join-Path -Path (Get-IcingaCacheDir) -ChildPath $Space) -ChildPath $CacheStore) -ChildPath ([string]::Format('{0}.json', $KeyName));
+    $cacheData = @{};
+
+    if ((Test-Path $CacheFile)) {
+        $cacheData = Get-IcingaCacheData -Space $Space -CacheStore $CacheStore;
+    } else {
+        New-Item -Path $CacheFile -Force | Out-Null;
+    }
+
+    if ($null -eq $cacheData -or $cacheData.Count -eq 0) {
+        $cacheData = @{
+            $KeyName = $Value
+        };
+    } else {
+        if ($cacheData.PSobject.Properties.Name -ne $KeyName) {
+            $cacheData | Add-Member -MemberType NoteProperty -Name $KeyName -Value $Value -Force;
+        } else {
+            $cacheData.$KeyName = $Value;
+        }
+    }
+
+    Set-Content -Path $CacheFile -Value (ConvertTo-Json -InputObject $cacheData -Depth 100) | Out-Null;
+}
