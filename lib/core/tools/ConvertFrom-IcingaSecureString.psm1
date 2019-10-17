317c42daef8e042be84524406214c6009afa921a
Lord Hepipud <contact@lordhepipud.de>
2019-10-17 09:57:16 +0200

    Added SecureString convertion functions

---
+++ lib/core/tools/ConvertFrom-IcingaSecureString.psm1
@@ -0,0 +1,13 @@
+function ConvertFrom-IcingaSecureString()
+{
+    param([SecureString]$SecureString);
+ 
+    if ($SecureString -eq $null) {
+        return '';
+    }
+
+    [IntPtr]$BSTR   = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+    [string]$String = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+    return $String;
+}
