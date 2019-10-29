401d08f66f66d93828c9d4889f709a146b7dd4b0
Crited <Alexander.Stoll@netways.de>
2019-10-29 11:34:59 +0100

    Documentation skeletons and actual documentation

--- lib/core/tools/Set-NumericNegative.psm1
+++ lib/core/tools/Set-NumericNegative.psm1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+   Sets nummeric values to be negative
+.DESCRIPTION
+   This module sets a numeric value to be negative.
+   e.g 12 to -12
+
+   More Information on https://github.com/LordHepipud/icinga-module-windows
+.EXAMPLE
+   PS> Set-NumericNegative 32
+   -32
+.LINK
+   https://github.com/LordHepipud/icinga-module-windows
+.NOTES
+#>
+
 
 function Set-NumericNegative()
 {
--- lib/core/tools/Test-Numeric.psm1
+++ lib/core/tools/Test-Numeric.psm1
@@ -1,3 +1,17 @@
+<#
+.SYNOPSIS
+   Tests whether a value is numeric
+.DESCRIPTION
+   This module tests whether a value is numeric
+
+   More Information on https://github.com/LordHepipud/icinga-module-windows
+.EXAMPLE
+   PS> Test-Numeric 32
+   True
+.LINK
+   https://github.com/LordHepipud/icinga-module-windows
+.NOTES
+#>
 function Test-Numeric ($number) {
     return $number -Match "^[\d\.]+$";
 }
