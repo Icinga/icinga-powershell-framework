0de69ee83077e0ea527d5fb5d562379411eb73ac
Lord Hepipud <contact@lordhepipud.de>
2019-10-31 13:43:20 +0100

    Add support to download and install PowerShell service

---
+++ lib/core/framework/Install-IcingaPowerShellService.psm1
@@ -0,0 +1,26 @@
+function Install-IcingaPowerShellService()
+{
+    param(
+        $Path,
+        $Username,
+        [SecureString]$Password
+    );
+
+    if ((Test-Path $Path) -eq $FALSE) {
+        throw 'Please specify the path directly to the service binary';
+    }
+
+    $Path = [string]::Format(
+        '{0} \"{1}\"',
+        $Path,
+        (Get-IcingaPowerShellModuleFile)
+    );
+
+    $ServiceCreation = Start-IcingaProcess -Executable 'sc.exe' -Arguments ([string]::Format('create icingapowershell binPath= "{0}" DisplayName= "Icinga PowerShell Service" start= auto', $Path));
+
+    if ($ServiceCreation.ExitCode -ne 0) {
+        throw ([string]::Format('Failed to install Icinga PowerShell Service: {0}{1}', $ServiceCreation.Message, $ServiceCreation.Error));
+    }
+
+    return (Set-IcingaAgentServiceUser -User $Username -Password $Password -Service 'icingapowershell');
+}
