34751548a22cbfeb8c0943d81215846118a2802a
Crited <astoll@netways.de>
2019-10-31 09:44:28 +0100

    Memory check uses Used Memory again (Auto-Detect: Draft for Unit Size)

--- lib/provider/memory/Get-IcingaMemoryPerformanceCounter.psm1
+++ lib/provider/memory/Get-IcingaMemoryPerformanceCounter.psm1
@@ -11,7 +11,7 @@ function Get-IcingaMemoryPerformanceCounter()
     $MemoryData.Add('Memory Available Bytes', [decimal]($Initial.'\Memory\Available Bytes'.value));
     $MemoryData.Add('Memory Total Bytes', (Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory);
     $MemoryData.Add('Memory Used Bytes', $MemoryData.'Memory Total Bytes' - $MemoryData.'Memory Available Bytes');
-    $MemoryData.Add('Memory Available %', 100 - ($MemoryData.'Memory Available Bytes' / $MemoryData.'Memory Total Bytes' * 100));
+    $MemoryData.Add('Memory Used %', 100 - ($MemoryData.'Memory Available Bytes' / $MemoryData.'Memory Total Bytes' * 100));
     $MemoryData.Add('PageFile %', $Initial.'\Paging File(_Total)\% usage'.value);
 
     return $MemoryData;
