80bd15e47feed7051c24c88cd29baceed4784534
Lord Hepipud <contact@lordhepipud.de>
2019-10-31 14:33:56 +0100

    Extend setup wizard with powershell framework service

--- lib/core/framework/Get-IcingaFrameworkServiceBinary.psm1
+++ lib/core/framework/Get-IcingaFrameworkServiceBinary.psm1
@@ -1,37 +1,37 @@
 function Get-IcingaFrameworkServiceBinary()
 {
     param(
-        [string]$DownloadUrl,
-        [string]$InstallDir
+        [string]$FrameworkServiceUrl,
+        [string]$ServiceDirectory
     );
 
     $ProgressPreference = "SilentlyContinue";
 
-    if ([string]::IsNullOrEmpty($DownloadUrl)) {
+    if ([string]::IsNullOrEmpty($FrameworkServiceUrl)) {
         if ((Get-IcingaAgentInstallerAnswerInput -Prompt 'Do you provide a custom source of the service binary?' -Default 'n').result -eq 1) {
-            $LatestRelease = (Invoke-WebRequest -Uri 'https://github.com/LordHepipud/icinga-windows-service/releases/latest' -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri;
-            $DownloadUrl = $LatestRelease.Replace('/tag/', '/download/');
-            $Tag         = $DownloadUrl.Split('/')[-1];
-            $DownloadUrl = [string]::Format('{0}/icinga-service-{1}.zip', $DownloadUrl, $Tag);
+            $LatestRelease       = (Invoke-WebRequest -Uri 'https://github.com/LordHepipud/icinga-windows-service/releases/latest' -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri;
+            $FrameworkServiceUrl = $LatestRelease.Replace('/tag/', '/download/');
+            $Tag                 = $FrameworkServiceUrl.Split('/')[-1];
+            $FrameworkServiceUrl = [string]::Format('{0}/icinga-service-{1}.zip', $FrameworkServiceUrl, $Tag);
         } else {
-            $DownloadUrl = (Get-IcingaAgentInstallerAnswerInput -Prompt 'Please enter the full path to your service binary repository' -Default 'v').answer;
+            $FrameworkServiceUrl = (Get-IcingaAgentInstallerAnswerInput -Prompt 'Please enter the full path to your service binary repository' -Default 'v').answer;
         }
     }
 
-    if ([string]::IsNullOrEmpty($InstallDir)) {
-        $InstallDir = (Get-IcingaAgentInstallerAnswerInput -Prompt 'Please enter the path you wish to install the service to' -Default 'v' -DefaultInput 'C:\Program Files\icinga-framework-service\').answer;
+    if ([string]::IsNullOrEmpty($ServiceDirectory)) {
+        $ServiceDirectory = (Get-IcingaAgentInstallerAnswerInput -Prompt 'Please enter the path you wish to install the service to' -Default 'v' -DefaultInput 'C:\Program Files\icinga-framework-service\').answer;
     }
 
-    if ((Test-Path $InstallDir) -eq $FALSE) {
-        New-Item -Path $InstallDir -Force -ItemType Directory | Out-Null;
+    if ((Test-Path $ServiceDirectory) -eq $FALSE) {
+        New-Item -Path $ServiceDirectory -Force -ItemType Directory | Out-Null;
     }
 
-    $ZipArchive = Join-Path -Path $InstallDir -ChildPath ($DownloadUrl.Split('/')[-1]);
-    $ServiceBin = Join-Path -Path $InstallDir -ChildPath 'icinga-service.exe';
+    $ZipArchive = Join-Path -Path $ServiceDirectory -ChildPath ($FrameworkServiceUrl.Split('/')[-1]);
+    $ServiceBin = Join-Path -Path $ServiceDirectory -ChildPath 'icinga-service.exe';
 
-    Invoke-WebRequest -Uri $DownloadUrl -UseBasicParsing -OutFile $ZipArchive;
+    Invoke-WebRequest -Uri $FrameworkServiceUrl -UseBasicParsing -OutFile $ZipArchive;
 
-    if ((Expand-IcingaZipArchive -Path $ZipArchive -Destination $InstallDir) -eq $FALSE) {
+    if ((Expand-IcingaZipArchive -Path $ZipArchive -Destination $ServiceDirectory) -eq $FALSE) {
         throw 'Failed to expand the downloaded ZIP archive';
     }
 
@@ -39,5 +39,9 @@ function Get-IcingaFrameworkServiceBinary()
         throw 'The checksum of the downloaded file and the required MD5 hash are not matching';
     }
 
-    return $ServiceBin;
+    return @{
+        'FrameworkServiceUrl' = $FrameworkServiceUrl;
+        'ServiceDirectory'    = $ServiceDirectory;
+        'ServiceBin'          = $ServiceBin;
+    };
 }
