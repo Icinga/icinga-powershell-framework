cb12600c3dbd6da33611f3507a9e6a5e59ad8bcf
Lord Hepipud <contact@lordhepipud.de>
2019-11-02 14:48:44 +0100

    Add function to combine web Urls properly

---
+++ lib/core/tools/Join-WebPath.psm1
@@ -0,0 +1,24 @@
+function Join-WebPath()
+{
+    param(
+        [string]$Path,
+        [string]$ChildPath
+    );
+
+    if ([string]::IsNullOrEmpty($Path) -Or [string]::IsNullOrEmpty($ChildPath)) {
+        return $Path;
+    }
+
+    [int]$Length = $Path.Length;
+    [int]$Slash  = $Path.LastIndexOf('/') + 1;
+
+    if ($Length -eq $Slash) {
+        $Path = $Path.Substring(0, $Path.Length - 1);
+    }
+
+    if ($ChildPath[0] -eq '/') {
+        return ([string]::Format('{0}{1}', $Path, $ChildPath));
+    }
+    
+    return ([string]::Format('{0}/{1}', $Path, $ChildPath));
+}
