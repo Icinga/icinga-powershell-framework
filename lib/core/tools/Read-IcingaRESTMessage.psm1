e6b46b84f7dc6f82cb2d0ea17ffbda2e0d04bf48
Alexander Stoll <45196568+Crited@users.noreply.github.com>
2020-03-24 18:43:27 +0100

    Fix codestyling

--- lib/core/tools/Read-IcingaRESTMessage.psm1
+++ lib/core/tools/Read-IcingaRESTMessage.psm1
@@ -4,8 +4,8 @@ function Read-IcingaRESTMessage()
         [string]$RestMessage = $null
     );
 
-    [hashtable]$Request = @{}
-    $RestMessage -match '(\d+) (.+) (.+) (.+)' | Out-Null
+    [hashtable]$Request = @{};
+    $RestMessage -match '(\d+) (.+) (.+) (.+)' | Out-Null;
  
     $Request.Add('MessageLength', $Matches[1]);
     $Request.Add('Method', $Matches[2]);
@@ -14,31 +14,31 @@ function Read-IcingaRESTMessage()
     $Request.Add('RequestArguments', @{});
 
     #Path
-    $PathMatch = $Matches[3]
+    $PathMatch = $Matches[3];
     $PathMatch -match '(.+)\?(.*)' | Out-Null
-    $Path      = $Matches[1]
-    $Arguments = $Matches[2]
+    $Path      = $Matches[1];
+    $Arguments = $Matches[2];
     $Request.RequestPath.Add('FullPath', $Matches[1]);
     $Request.RequestPath.Add('PathArray', $Matches[1].TrimStart('/').Split('/'));
 
-    $Matches = $null
+    $Matches = $null;
 
     # Arguments
-    $ArgumentsSplit = $Arguments.Split('&')
-    $ArgumentsSplit+='\\\\\\\\\\\\=FIN'
+    $ArgumentsSplit = $Arguments.Split('&');
+    $ArgumentsSplit+='\\\\\\\\\\\\=FIN';
     foreach ( $Argument in $ArgumentsSplit | sort -descending) {
-        $Argument -match '(.+)=(.+)' | Out-Null
+        $Argument -match '(.+)=(.+)' | Out-Null;
         If (($Matches[1] -ne $Current) -And ($NULL -ne $Current)) {
             $Request.RequestArguments.Add( $Current, $ArgumentContent );
-            [array]$ArgumentContent = $null
+            [array]$ArgumentContent = $null;
         }
-        $Current = $Matches[1]
+        $Current = $Matches[1];
         [array]$ArgumentContent+=($Matches[2]);
     }
 
     # Header
     $Request.Add( 'Header', @{} );
-    $SplitString = $RestMessage.split("`r`n")
+    $SplitString = $RestMessage.split("`r`n");
     foreach ( $SingleString in $SplitString ) {
         if ( ([string]::IsNullOrEmpty($SingleString) -eq $FALSE) -And ($SingleString -match '^{.+' -eq $FALSE) ) {
             $SingleSplitString = $SingleString.Split(':',2);
@@ -48,10 +48,10 @@ function Read-IcingaRESTMessage()
 
     $Request.Add('ContentLength', [int](Get-IcingaRESTHeaderValue -Header 'Content-Length' -Request $Request));
 
-    $Matches = $null
+    $Matches = $null;
 
     # Body
-    $RestMessage -match '(\{(.*\n)*}|\{.*\})' | Out-Null
+    $RestMessage -match '(\{(.*\n)*}|\{.*\})' | Out-Null;
     $Request.Add('Body', $Matches[1]);
 
     return $Request;
