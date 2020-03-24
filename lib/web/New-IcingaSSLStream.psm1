8d945f3759b27822ef081e61f95c5208d1b17984
Christian Stein <cstein@netways.de>
2020-03-24 20:13:28 +0100

    Fixes client initialising for SSLStream and possible crash reason

--- lib/web/New-IcingaSSLStream.psm1
+++ lib/web/New-IcingaSSLStream.psm1
@@ -1,9 +1,13 @@
 function New-IcingaSSLStream()
 {
     param(
-        [System.Net.Sockets.TcpClient]$Client                                 = @{},
+        [System.Net.Sockets.TcpClient]$Client                                 = $null,
         [Security.Cryptography.X509Certificates.X509Certificate2]$Certificate = $null
     );
+
+    if ($null -eq $Client) {
+        return $null;
+    }
 
     $SSLStream = New-Object System.Net.Security.SslStream($Client.GetStream(), $false)
     $SSLStream.AuthenticateAsServer($Certificate, $false, [System.Security.Authentication.SslProtocols]::Tls12, $true) | Out-Null;
