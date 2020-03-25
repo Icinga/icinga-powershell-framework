abb2ab1e35ea67cbee3cc008219560810df84082
Christian Stein <cstein@netways.de>
2020-03-25 17:50:02 +0100

    Adds function for simple navigation through REST-Api calls

---
+++ lib/web/Get-IcingaRESTPathElement.psm1
@@ -0,0 +1,21 @@
+function Get-IcingaRESTPathElement()
+{
+    param(
+        [Hashtable]$Request = @{},
+        [int]$Index         = 0
+    );
+
+    if ($null -eq $Request -Or $Request.Count -eq 0) {
+        return '';
+    }
+
+    if ($Request.ContainsKey('RequestPath') -eq $FALSE) {
+        return '';
+    } 
+
+    if (($Index + 1) -gt $Request.RequestPath.PathArray.Count) {
+        return '';
+    }
+
+    return $Request.RequestPath.PathArray[$Index];
+}
