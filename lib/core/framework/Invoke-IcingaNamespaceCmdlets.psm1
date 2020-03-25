2e225e1ce5c20c70a0099b91025d75e65704bd09
Christian Stein <cstein@netways.de>
2020-03-25 17:51:53 +0100

    Adds support for generic call of wildcard functions for fetching multi data

---
+++ lib/core/framework/Invoke-IcingaNamespaceCmdlets.psm1
@@ -0,0 +1,30 @@
+function Invoke-IcingaNamespaceCmdlets()
+{
+    param (
+        [string]$Command
+    );
+
+    [Hashtable]$CommandConfig = @{};
+
+    if ($Command.Contains('*') -eq $FALSE) {
+        $Command = [string]::Format('{0}*', $Command);
+    }
+
+    $CommandList = Get-Command $Command;
+
+    foreach ($Cmdlet in $CommandList) {
+        try {
+            $CmmandName = $Cmdlet.Name;
+            Import-Module $Cmdlet.Module.Path -WarningAction SilentlyContinue -ErrorAction Stop;
+
+            $Content = (& $CmmandName);
+            Add-IcingaHashtableItem -Hashtable $CommandConfig `
+                                    -Key $Cmdlet.Name `
+                                    -Value $Content | Out-Null;
+        } catch {
+            # TODO: Add event log logging on exceptions
+        }
+    }
+
+    return $CommandConfig;
+}
