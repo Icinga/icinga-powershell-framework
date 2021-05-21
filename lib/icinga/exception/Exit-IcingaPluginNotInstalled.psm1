47203d39c5e4a10ddbd2a43eb98ef7bc2f069ec5
Lord Hepipud <contact@lordhepipud.de>
2021-05-21 11:02:51 +0200

    Feature: Inform the user if plugins are executed which are not meeting dependency requirements Fixes #250

--- lib/icinga/exception/Exit-IcingaPluginNotInstalled.psm1
+++ lib/icinga/exception/Exit-IcingaPluginNotInstalled.psm1
@@ -24,8 +24,26 @@
 function Exit-IcingaPluginNotInstalled()
 {
     param (
-        $Command
+        [string]$Command
     );
+
+    $PowerShellModule = Get-Module 'icinga-powershell-*' -ListAvailable |
+        ForEach-Object {
+            foreach ($cmd in $_.ExportedCommands.Values) {
+                if ($Command.ToLower() -eq $cmd.Name.ToLower()) {
+                    return $cmd.Source;
+                }
+            }
+        }
+
+    if ([string]::IsNullOrEmpty($PowerShellModule) -eq $FALSE) {
+        try {
+            Import-Module $PowerShellModule -ErrorAction Stop;
+        } catch {
+            $ExMsg = $_.Exception.Message;
+            Exit-IcingaThrowException -CustomMessage 'Module not loaded' -ExceptionType 'Configuration' -ExceptionThrown $ExMsg -Force;
+        }
+    }
 
     if ([string]::IsNullOrEmpty($Command)) {
         Exit-IcingaThrowException -CustomMessage 'Null-Command' -ExceptionType 'Configuration' -ExceptionThrown $IcingaExceptions.Configuration.PluginNotAssigned -Force;
