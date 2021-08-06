351848dc6f80013a27fc812e762803fc92160e86
Lord Hepipud <contact@lordhepipud.de>
2021-08-06 13:54:47 +0200

    Relocating reconfigure menu entry

--- lib/core/installer/menu/installation/general/Show-IcingaForWindowsInstallerMenuInstallWindows.psm1
+++ lib/core/installer/menu/installation/general/Show-IcingaForWindowsInstallerMenuInstallWindows.psm1
@@ -8,7 +8,7 @@ function Show-IcingaForWindowsInstallerMenuInstallWindows()
         [switch]$Advanced      = $FALSE
     );
 
-    if ($null -eq (Get-IcingaPowerShellConfig -Path 'Framework.Config.Swap')) {
+    if ($null -eq (Get-IcingaPowerShellConfig -Path 'Framework.Config.Swap') -And $null -eq (Get-IcingaPowerShellConfig -Path 'Framework.Config.Live')) {
         Show-IcingaForWindowsInstallerMenuSelectConnection;
         return;
     }
@@ -22,9 +22,16 @@ function Show-IcingaForWindowsInstallerMenuInstallWindows()
                 'Help'    = 'Start a new configuration and truncate all information stored on the current swap file. This will only modify your production if you hit "Start installation" at the end';
             },
             @{
-                'Caption' = 'Continue configuration';
-                'Command' = 'Show-IcingaForWindowsInstallerMenuContinueConfiguration';
-                'Help'    = 'Continue with the previous configuration swap file.';
+                'Caption'  = 'Continue configuration';
+                'Command'  = 'Show-IcingaForWindowsInstallerMenuContinueConfiguration';
+                'Help'     = 'Continue with the previous configuration swap file.';
+                'Disabled' = ([bool]($null -eq (Get-IcingaPowerShellConfig -Path 'Framework.Config.Swap')));
+            },
+            @{
+                'Caption'  = 'Reconfigure Environment';
+                'Command'  = 'Invoke-IcingaForWindowsManagementConsoleReconfigureAgent';
+                'Help'     = 'Load the current configuration of Icinga for Windows to modify it.';
+                'Disabled' = ([bool]($null -eq (Get-IcingaPowerShellConfig -Path 'Framework.Config.Live')));
             }
         ) `
         -DefaultIndex $DefaultInput `
