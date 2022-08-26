72157d3a7fe6a02b3db22c997cbb012396d763b7
Lord Hepipud <contact@lordhepipud.de>
2022-08-26 15:36:52 +0200

    Fixes exception on update file removal

--- lib/core/installer/tools/SetUpdatingIMC.psm1
+++ lib/core/installer/tools/SetUpdatingIMC.psm1
@@ -8,7 +8,9 @@ function Set-IcingaForWindowsManagementConsoleUpdating()
 
     $UpdateFile = Join-Path -Path (Get-IcingaCacheDir) -ChildPath 'framework.update';
     if ($Completed) {
-        Remove-ItemSecure -Path $UpdateFile -Force -Retries 5 | Out-Null;
+        if (Test-IcingaForWindowsManagementConsoleUpdating) {
+            Remove-ItemSecure -Path $UpdateFile -Force -Retries 5 | Out-Null;
+        }
     } else {
         New-Item -Path $UpdateFile -ItemType File -Force | Out-Null;
     }
