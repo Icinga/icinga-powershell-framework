307a57a5bf231996c98dcd59bdcf0cc71418cd05
Lord Hepipud <contact@lordhepipud.de>
2024-05-15 17:21:30 +0200

    Adds feature to check Eventlog for problem and acknowleding events

--- lib/provider/logging/Get-IcingaProviderDataValuesEventlog.psm1
+++ lib/provider/logging/Get-IcingaProviderDataValuesEventlog.psm1
@@ -12,6 +12,7 @@ function Get-IcingaProviderDataValuesEventlog()
 
     $EventLogData.Metrics | Add-Member -MemberType NoteProperty -Name 'List'      -Value $FilterObject.EventLog.Query.List;
     $EventLogData.Metrics | Add-Member -MemberType NoteProperty -Name 'Events'    -Value $FilterObject.EventLog.Query.Events;
+    $EventLogData.Metrics | Add-Member -MemberType NoteProperty -Name 'Problems'  -Value $FilterObject.EventLog.Query.Problems;
     $EventLogData.Metrics | Add-Member -MemberType NoteProperty -Name 'HasEvents' -Value $FilterObject.EventLog.Query.HasEvents;
 
     $FilterObject = $null;
