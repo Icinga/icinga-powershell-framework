7a046258efffc1a4ce057045093ec8797125b085
Lord Hepipud <contact@lordhepipud.de>
2025-01-29 15:43:24 +0100

    Improves service recover by adjustable restart time interval argument

--- lib/core/windows/Enable-IcingaServiceRecovery.psm1
+++ lib/core/windows/Enable-IcingaServiceRecovery.psm1
@@ -1,7 +1,14 @@
 function Enable-IcingaServiceRecovery()
 {
+    param (
+        [int]$IntervalInSeconds = 120
+    );
+
+    # The interval in milliseconds to restart actions to happen for the service
+    $IntervalInMilliseconds = $IntervalInSeconds * 1000;
+
     if ($null -ne (Get-Service 'icinga2' -ErrorAction SilentlyContinue)) {
-        $ServiceStatus = Start-IcingaProcess -Executable 'sc.exe' -Arguments 'failure icinga2 reset=0 actions=restart/0/restart/0/restart/0';
+        $ServiceStatus = Start-IcingaProcess -Executable 'sc.exe' -Arguments ([string]::Format('failure icinga2 reset=0 actions=restart/{0}/restart/{0}/restart/{0}', $IntervalInMilliseconds));
 
         if ($ServiceStatus.ExitCode -ne 0) {
             Write-IcingaConsoleError -Message 'Failed to enable recover settings for service "icinga2": {0} {1}' -Objects $ServiceStatus.Message, $ServiceStatus.Error;
@@ -11,7 +18,7 @@ function Enable-IcingaServiceRecovery()
     }
 
     if ($null -ne (Get-Service 'icingapowershell' -ErrorAction SilentlyContinue)) {
-        $ServiceStatus = Start-IcingaProcess -Executable 'sc.exe' -Arguments 'failure icingapowershell reset=0 actions=restart/0/restart/0/restart/0';
+        $ServiceStatus = Start-IcingaProcess -Executable 'sc.exe' -Arguments ([string]::Format('failure icingapowershell reset=0 actions=restart/{0}/restart/{0}/restart/{0}', $IntervalInMilliseconds));
 
         if ($ServiceStatus.ExitCode -ne 0) {
             Write-IcingaConsoleError -Message 'Failed to enable recover settings for service "icingapowershell": {0} {1}' -Objects $ServiceStatus.Message, $ServiceStatus.Error;
