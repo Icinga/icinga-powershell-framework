416d85dc893b823d14861e649ca403e7a84fd112
Lord Hepipud <contact@lordhepipud.de>
2026-02-02 10:25:14 +0100

    Adds migration tasks to v1.12.5

--- lib/core/framework/Invoke-IcingaForWindowsMigration.psm1
+++ lib/core/framework/Invoke-IcingaForWindowsMigration.psm1
@@ -156,4 +156,14 @@ function Invoke-IcingaForWindowsMigration()
 
         Set-IcingaForWindowsMigration -MigrationVersion (New-IcingaVersionObject -Version '1.12.4');
     }
+
+    if (Test-IcingaForWindowsMigration -MigrationVersion (New-IcingaVersionObject -Version '1.12.5')) {
+        Write-IcingaConsoleNotice 'Applying pending migrations required for Icinga for Windows v1.12.5';
+
+        # Fixes potential permission issues with the Icinga Service User and the SeServiceLogonRight privilege
+        $ServiceUser = Get-IcingaServiceUser;
+        Set-IcingaUserPermissions -IcingaUser $ServiceUser;
+
+        Set-IcingaForWindowsMigration -MigrationVersion (New-IcingaVersionObject -Version '1.12.5');
+    }
 }
